d7f60460b192ef126793dfaa0d1bcd60a4e38b28
Ianna <ianna1014@gmail.com>
2020-09-02 16:04:38 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int age = 19;
+        String city = "Philadelphia";
+        float gpa =3.95f;
+        boolean school = false;
+
+        System.out.println("My name is Ianna and I am "+ age +" years old.");
+        System.out.println("I am from " + city + ".");
+        System.out.println("While at Howard I have been able to maintain a " + gpa + " GPA.");
+        System.out.println("Do you attend Hampton ? " + school + "!!");
+
+    }
+}
